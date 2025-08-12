ccc7667f8febe58693e23a421ed3d0c2c1442e7c
eppalapallysharath <eppalapallysharath@gmail.com>
2025-08-12 18:17:06 +0530

    - Developed APIS for managers and employees based on their actions

--- Controllers/employeeController.js
+++ Controllers/employeeController.js
@@ -1,9 +1,42 @@
-exports.viewAssignedTickets = (req, res, next) => {
-  res.send("view assigned tickets");
+const { TaskModel } = require("../Models/taskModel.js");
+exports.viewAssignedTickets = async (req, res, next) => {
+  try {
+    const userId = req.userInfo._id;
+    const allTickets = await TaskModel.find()
+      .where({ assignTo: userId })
+      .populate("createdBy", ["name", "username"]);
+    res.status(200).json({ message: "your assigned tasks", data: allTickets });
+  } catch (error) {
+    console.log(error);
+    const err = { statusCode: 400, message: "something went wrong" };
+    next(err);
+  }
 };
 
-exports.updateTicketStatusById = (req, res, next) => {
-  res.send("update tickets status");
+exports.updateTicketStatusById = async (req, res, next) => {
+  try {
+    const { taskStatus } = req.body;
+    const taskId = req.params.ticketID;
+    const userId = req.userInfo._id;
+    const task = await TaskModel.findByIdAndUpdate(
+      taskId,
+      {
+        status: taskStatus,
+      },
+      { new: true }
+    ).where({
+      assignTo: userId,
+    });
+    if (task) {
+      return res.json({ message: "Task updated successfully", data: task });
+    } else {
+      return res.status(404).json({ message: "No task found" });
+    }
+  } catch (error) {
+    console.log(error);
+    const err = { statusCode: 400, message: "something went wrong" };
+    next(err);
+  }
 };
 
 exports.addCommentToTicketById = (req, res, next) => {
